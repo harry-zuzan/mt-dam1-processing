f3d9513007d94d68ed2e8a44f2776efd01abfeae
Harry Zuzan <iliketobicycle@gmail.com>
2016-05-10 10:25:34 -0700

    added write-shrunk-img-freq.sh

---
+++ write-shrunk-img-freq.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+/home/harry/bin/write-image-power.py \
+--group Shrunk \
+--fourier-data-set "fourier image" \
+--image-path image-shrunk \
+--image-name raw-stack-shrunk-freq \
+data-hdf5/raw-stack00000.hdf5
+
