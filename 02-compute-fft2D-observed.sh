41ff716a256156efebe1beb0059338041d482ce2
Harry Zuzan <iliketobicycle@gmail.com>
2016-05-10 16:02:16 -0700

    adding more scripts

--- 02-compute-fft2D-observed.sh
+++ 02-compute-fft2D-observed.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# computes the 2D fourier transform of the data in the observed group
+
 cryoem-compute-fft2D.py data-hdf5/raw-stack*.hdf5
 
--- 05-compute-polar-pwr-observed.sh
+++ 05-compute-polar-pwr-observed.sh
@@ -3,11 +3,10 @@
 # computes the polar image of the fourier transform
 
 cryoem-compute-polar-ft-pwr.py \
---group Shrunk \
 --polar-rows 256 \
 --polar-cols 512 \
 --polar-radius 0.65 \
-data-hdf5/raw-stack00000.hdf5
+data-hdf5/raw-stack*.hdf5
 
 
 
