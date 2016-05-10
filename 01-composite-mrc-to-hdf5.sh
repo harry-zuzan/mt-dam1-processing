d9f33fe9ed29c0c74220993d74d252575a6da8ea
Harry Zuzan <iliketobicycle@gmail.com>
2016-05-10 12:11:09 -0700

    deleted unused lines that were commented out

--- 01-composite-mrc-to-hdf5.sh
+++ 01-composite-mrc-to-hdf5.sh
@@ -5,10 +5,4 @@ cryoem-composite-mrc-to-hdf5.py \
 --root-name raw-stack \
 ./all_pf_raw_stack.mrc
 
-#/home/harry/bin/write-image-power.py \
-#--group Shrunk \
-#--fourier-data-set "fourier image" \
-#--image-path image-shrunk \
-#--image-name raw-stack-shrunk-freq \
-#data-hdf5/raw-stack00000.hdf5
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mt-dam1-processing
 Processing of the cryo-em microtubule dam1 ring complex data set
+
+Shell scripts are hard coded to run python scripts for the purpose of
+running, recording and being able to repeat a workflow
+
